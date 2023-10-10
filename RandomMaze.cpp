8dc11dbb9bd1742f16832dc237f48c0520f7a169
Tanisha <s3975655@student.rmit.edu.au>
2023-10-11 09:08:52 +1100

    test commit tanisha

---
+++ RandomMaze.cpp
@@ -0,0 +1,14 @@
+/* Generate Random Maze:
+    1. Take input from user (base point, length and width).
+    2. Check for valid inputs.
+    3. Recursive Backtracking Algorithm
+    4. Output Message
+*/
+#include <iostream>
+#include "Maze.cpp"
+
+using std::cin; 
+using std::cout;
+using std::endl;
+
+String message
